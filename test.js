c42b970c8f5f0f133459beb55d9c7b12e12e1a3b
robin-5799 <robinbny5799@gmail.com>
2022-03-29 23:36:14 +0530

    variable

--- test.js
+++ test.js
@@ -1,1 +1,32 @@
 console.log("hello js")
+// var a=3;
+// console.log("test value of a",a);
+// a="new";
+// console.log("updated value", a);
+
+// let a=23;
+// console.log("value of a", a);
+// a=25;
+// console.log("new value",a);
+// let varA="ssss";
+// var varB="AA";
+// const varC=" this is const";
+
+// console.log("value1",varA);
+// console.log("value2",varB);
+// console.log("value3",varC);
+
+// varA="new value";
+// varB="updated value";
+
+// console.log("value1",varA);
+// console.log("value2",varB);
+// console.log("value3",varC);
+
+const a=2;
+console.log("value of A",a);
+
+test();
+function test(){
+    console.log("this is a test function");
+}
