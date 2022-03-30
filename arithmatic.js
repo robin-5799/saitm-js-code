a1e1a5c3f8a36c3c3b40ef02ac25a5659d52e631
robin-5799 <robinbny5799@gmail.com>
2022-03-30 23:36:54 +0530

    functions

---
+++ arithmatic.js
@@ -0,0 +1,101 @@
+
+
+// var add=2+2;
+// console.log("add equals to",add);
+
+// var sub=2-2;
+// console.log("subtract equals to",sub);
+
+// var mul=2*2;
+// console.log("multiply equals to",mul);
+
+// var div=2/2;
+// console.log("division equals to",div);
+
+// var rem=5%2;
+// console.log("reminder",rem);
+
+// var pow=2**2;
+// console.log("power equals to",pow);
+
+// var a=5;
+// console.log("value",++a);
+// console.log("value",a++);
+// console.log("value",a);
+
+// var a=5;
+// var b=2;
+
+// console.log("statement", a>b ? true : false);
+
+// var month="jan";
+// var ariMonth;
+
+// switch(month){
+//     case "jan":console.log("January");
+//                break;
+//     case "feb":console.log("February");
+//     case "dec":console.log("December");
+//     default: console.log("Random");
+// }
+
+// for(let i=0;i<4;i++){
+//     console.log(i);
+// }
+
+// var arr=[2,3,5,6,7];
+
+// for(let i of arr){
+//     console.log("array",i);
+// }
+
+// var n=3;
+// for(let i=1;i<=n;i++){
+//     var space=n-i;
+//     for(let j=1;j<=n;j++){
+//         console.log(" ",'*',"\n")
+//     }
+// }
+
+// function add(a,b){
+//     console.log("add is ",a+b);
+// }
+// add(2,3);
+
+// var j=1;
+// while(j<10){
+//     console.log("value",j);
+//     j=j+2;
+// }
+
+// var k=5;
+// do{
+//     console.log("at least",k);
+//     k--;
+// }while(k<0);
+
+// var counter=10;
+
+// for(let i=0;i<6;i++){
+//     if(i==3){  
+//         continue;
+//     }
+//     counter+=1;
+//     console.log("counter is",counter);
+// }
+
+// function add(a,b){
+//     return a+b;
+// }
+// var total=add(2,3);
+// console.log("add is",total);
+
+// var total2=(a,b)=>{
+//     return a+b;
+// }
+// console.log("add2 is",total2(2,3));
+
+ var mul=function(a,b){
+    return a*b;
+}
+console.log("multiply is", mul(3,5));
